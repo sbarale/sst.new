46ff4a455979275d36e7fb072a3671edc4f44e5d
Sebastian Barale <sebasinar@gmail.com>
2018-08-13 19:22:59 -0300

    asd

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,10 +1,10 @@
 @servers(['web' => 'root@smartsavings.today'])
 
 @task('deploy')
-    cd /storage/vol1/sites/smartsavings.today/
-    git pull origin master
+cd /storage/vol1/sites/smartsavings.today/
+git pull origin master
 @endtask
 
 @finished
-@slack('https://hooks.slack.com/services/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', '#general')
+@slack('https://hooks.slack.com/services/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', '#general','New deployment done!')
 @endfinished
