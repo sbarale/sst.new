18f23589cbba370af3669ca8125398d117c81f6c
Sebastian Barale <sebasinar@gmail.com>
2018-08-13 19:19:39 -0300

    asd

---
+++ Envoy.blade.php
@@ -0,0 +1,10 @@
+@servers(['web' => 'root@smartsavings.today'])
+
+@task('deploy')
+    cd /storage/vol1/sites/smartsavings.today/
+    git pull origin master
+@endtask
+
+@finished
+@slack('https://hooks.slack.com/services/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', '#general')
+@endfinished
