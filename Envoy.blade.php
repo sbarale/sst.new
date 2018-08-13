ab27698a739768b167756e2559b4d195ca0fac35
Sebastian Barale <sebasinar@gmail.com>
2018-08-13 19:24:56 -0300

    asd

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,10 +1,52 @@
 @servers(['web' => 'root@smartsavings.today'])
 
-@task('deploy')
-cd /storage/vol1/sites/smartsavings.today/
-git pull origin master
+@setup
+$project_name = 'Smart Savings Today';
+$project_url = 'http://smartsavings.today/';
+$project_root = '/storage/vol1/sites/smartsavings.today/';
+
+$slack_hook = 'https://hooks.slack.com/services/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';
+$slack_channel = '#general';
+@endsetup
+
+@task('put_app_up', ['on' => 'web'])
+cd {{ $project_root }}
+
+php artisan up
 @endtask
 
-@finished
-@slack('https://hooks.slack.com/services/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', '#general','New deployment done!')
-@endfinished
+@task('put_app_down', ['on' => 'web'])
+cd {{ $project_root }}
+
+php artisan down
+@endtask
+
+@task('pull_latest_changes', ['on' => 'web'])
+cd {{ $project_root }}
+
+git pull origin
+@endtask
+
+@task('install_dependencies', ['on' => 'web'])
+cd {{ $project_root }}
+
+composer install  --prefer-source --no-interaction --no-dev
+@endtask
+
+@task('clear_cache', ['on' => 'web'])
+cd {{ $project_root }}
+
+php artisan cache:clear
+@endtask
+
+@macro('update')
+put_app_down
+pull_latest_changes
+install_dependencies
+put_app_up
+@endmacro
+
+@after
+@slack($slack_hook, $slack_channel, "Envoy task $task ran on <$project_url|[$project_name]>")
+@endafter
+
