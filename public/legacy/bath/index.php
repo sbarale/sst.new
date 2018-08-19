f74eda93dc68d3ac44ec2ecf6e82cdf3d8d12af1
Sebastian Barale <sebasinar@gmail.com>
2018-08-19 09:42:06 -0700

    changed to use autocomplete

--- public/legacy/bath/index.php
+++ public/legacy/bath/index.php
@@ -21,22 +21,22 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Bathwraps</title>
     <!--    <link href="./files/css.css" rel="stylesheet">-->
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-    <script type="text/javascript" src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/web.min.css">
     <link rel="stylesheet" type="text/css" href="./files/style.css">
 
     <link rel="stylesheet" href="css/navigation.css">
-    <script type="text/javascript" src="http://parsleyjs.org/dist/parsley.js"></script>
-    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+    <script type="text/javascript" src="//parsleyjs.org/dist/parsley.js"></script>
+    <link href="//fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
 </head>
 <body>
 <!-- Google Tag Manager (noscript) -->
 <noscript>
-    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M2H3LLJ"
+    <iframe src="//www.googletagmanager.com/ns.html?id=GTM-M2H3LLJ"
             height="0" width="0" style="display:none;visibility:hidden"></iframe>
 </noscript>
 <!-- End Google Tag Manager (noscript) -->
