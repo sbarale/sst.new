9a1ce53e1dd3cc84dd9fd906638da8a489a2084e
Sebastian Barale <sebasinar@gmail.com>
2018-08-19 10:38:38 -0700

    Added step

--- public/legacy/bath/js/navigation.js
+++ public/legacy/bath/js/navigation.js
@@ -12,6 +12,8 @@ $(function () {
         var atTheEnd = index >= $sections.length - 1;
         $('.form-navigation .next').toggle(!atTheEnd);
         $('.form-navigation [type=submit]').toggle(atTheEnd);
+        dataLayer.push({ 'event': 'step_' + index });
+
     }
 
     function curIndex() {
