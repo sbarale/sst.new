6fdc339129ec06dfe7de75809249a9e2f63cba90
Sebastian Barale <sebasinar@gmail.com>
2018-11-06 07:13:52 -0300

    Added GTM

--- resources/views/layouts/slider-aqua.blade.php
+++ resources/views/layouts/slider-aqua.blade.php
@@ -2,41 +2,42 @@
 <html lang="en">
 <head>
     @section('head')
-    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <title>@yield('title','Take the Quiz to See if you Qualify')</title>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <title>@yield('title','Take the Quiz to See if you Qualify')</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
 
-    <link href="/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <link href='//fonts.googleapis.com/css?family=Merriweather:regular,700' rel='stylesheet' type='text/css' />
-    <script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js'></script>
-    <script type='text/javascript' src='//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js'></script>
-    <link href='//fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css' />
-    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC9xy8KF8H9Cu-Bui1nB5zHM860PlgkFFw&libraries=places,geometry"></script>
-    <link rel="stylesheet" href="/css/bootstrap-tagsinput.css" type="text/css">
+        <link href="/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href='//fonts.googleapis.com/css?family=Merriweather:regular,700' rel='stylesheet' type='text/css'/>
+        <script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js'></script>
+        <script type='text/javascript' src='//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js'></script>
+        <link href='//fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'/>
+        <script type="text/javascript"
+                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC9xy8KF8H9Cu-Bui1nB5zHM860PlgkFFw&libraries=places,geometry"></script>
+        <link rel="stylesheet" href="/css/bootstrap-tagsinput.css" type="text/css">
 
-    <script type="text/javascript" src="/js/jquery.maskedinput.min.js"></script>
-    <link href="/css/jquery.nouislider.css" rel="stylesheet" type='text/css' />
-    <style>
-        body{
-            background-image: url(@yield('big-bg-img','/images/bath/2/background_2.jpg'));
-            -webkit-background-size: cover;
-            background-size: cover;
-        }
-    </style>
+        <script type="text/javascript" src="/js/jquery.maskedinput.min.js"></script>
+        <link href="/css/jquery.nouislider.css" rel="stylesheet" type='text/css'/>
+        <style>
+            body {
+                background-image: url(@yield('big-bg-img','/images/bath/2/background_2.jpg'));
+                -webkit-background-size: cover;
+                background-size: cover;
+            }
+        </style>
 
     @show
 
     @section('scripts')
-
+        @include('trackers.gtm')
     @show
 </head>
 <body class="graybg">
 @include('fronts.sst._common.debugbar')
-<header class="navbar navbar-static-top" id="top" role="banner" >
+<header class="navbar navbar-static-top" id="top" role="banner">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
@@ -47,7 +48,8 @@
                                 <a href="/"><img src="/images/new-logo.png" class=" img-responsive pull-left"/></a>
                             </div>
                             <div class="col-md-4 hidden-xs hidden-sm">
-                                <img src="@yield('header-middle-img','/images/gray-bg-px.jpg')" class="img-responsive center-block"/>
+                                <img src="@yield('header-middle-img','/images/gray-bg-px.jpg')"
+                                     class="img-responsive center-block"/>
                             </div>
                             <div class="col-md-4 hidden-xs">
                                 <img src="/images/privacy-logo-black.png" class="img-responsive pull-right secure-img"/>
@@ -74,33 +76,33 @@
     @include('fronts.sst.footer')
 </div>
 @section('footer-scripts')
-<script type='text/javascript' src='/js/jquery.nouislider.all.min.js'></script>
-<script type="text/javascript" src="/js/moment.min.js"></script>
-<script type="text/javascript" src="/js/parsley.min.js"></script>
-<script type="text/javascript" src="/js/formslider.js"></script>
-<script type="text/javascript" src="/js/jquery.autotab.js"></script>
-<script src="/js/bootstrap-tagsinput.min.js"></script>
-<script src="/js/bootstrap-typeahead.js"></script>
+    <script type='text/javascript' src='/js/jquery.nouislider.all.min.js'></script>
+    <script type="text/javascript" src="/js/moment.min.js"></script>
+    <script type="text/javascript" src="/js/parsley.min.js"></script>
+    <script type="text/javascript" src="/js/formslider.js"></script>
+    <script type="text/javascript" src="/js/jquery.autotab.js"></script>
+    <script src="/js/bootstrap-tagsinput.min.js"></script>
+    <script src="/js/bootstrap-typeahead.js"></script>
 
-<!--  leadactiveprospect dot com -->
-<script type="text/javascript">
-    (function () {
-            var field = 'xxTrustedFormCertUrl';
-            var provideReferrer = false;
-            var tf = document.createElement('script');
-            tf.type = 'text/javascript';
-            tf.async = true;
-            tf.src = 'http' + ('https:' == document.location.protocol ? 's' : '') +
-                '://api.trustedform.com/trustedform.js?provide_referrer=' + escape(provideReferrer) + '&field=' + escape(field) + '&l=' + new Date().getTime() + Math.random();
-            var s = document.getElementsByTagName('script')[0];
-            s.parentNode.insertBefore(tf, s);
-        }
-    )();
-</script>
-<noscript>
-    <img src="https://api.trustedform.com/ns.gif" />
-</noscript>
-<!--  end leadactiveprospect dot com -->
+    <!--  leadactiveprospect dot com -->
+    <script type="text/javascript">
+        (function () {
+                var field = 'xxTrustedFormCertUrl';
+                var provideReferrer = false;
+                var tf = document.createElement('script');
+                tf.type = 'text/javascript';
+                tf.async = true;
+                tf.src = 'http' + ('https:' == document.location.protocol ? 's' : '') +
+                    '://api.trustedform.com/trustedform.js?provide_referrer=' + escape(provideReferrer) + '&field=' + escape(field) + '&l=' + new Date().getTime() + Math.random();
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(tf, s);
+            }
+        )();
+    </script>
+    <noscript>
+        <img src="https://api.trustedform.com/ns.gif"/>
+    </noscript>
+    <!--  end leadactiveprospect dot com -->
 @show
 
 </body>
--- resources/views/layouts/slider.blade.php
+++ resources/views/layouts/slider.blade.php
@@ -20,17 +20,7 @@
 
         <script type="text/javascript" src="/js/jquery.maskedinput.min.js"></script>
         <link href="/css/jquery.nouislider.css" rel="stylesheet" type='text/css' />
-        <!-- Start Visual Website Optimizer Asynchronous Code -->
-        <script type='text/javascript'>
-            var _vwo_code=(function(){
-                var account_id=374267,
-                    settings_tolerance=2000,
-                    library_tolerance=2500,
-                    use_existing_jquery=false,
-                    /* DO NOT EDIT BELOW THIS LINE */
-                    f=false,d=document;return{use_existing_jquery:function(){return use_existing_jquery;},library_tolerance:function(){return library_tolerance;},finish:function(){if(!f){f=true;var a=d.getElementById('_vis_opt_path_hides');if(a)a.parentNode.removeChild(a);}},finished:function(){return f;},load:function(a){var b=d.createElement('script');b.src=a;b.type='text/javascript';b.innerText;b.onerror=function(){_vwo_code.finish();};d.getElementsByTagName('head')[0].appendChild(b);},init:function(){settings_timer=setTimeout('_vwo_code.finish()',settings_tolerance);var a=d.createElement('style'),b='body{opacity:0 !important;filter:alpha(opacity=0) !important;background:none !important;}',h=d.getElementsByTagName('head')[0];a.setAttribute('id','_vis_opt_path_hides');a.setAttribute('type','text/css');if(a.styleSheet)a.styleSheet.cssText=b;else a.appendChild(d.createTextNode(b));h.appendChild(a);this.load('//dev.visualwebsiteoptimizer.com/j.php?a='+account_id+'&u='+encodeURIComponent(d.URL)+'&r='+Math.random());return settings_timer;}};}());_vwo_settings_timer=_vwo_code.init();
-        </script>
-        <!-- End Visual Website Optimizer Asynchronous Code -->
+
         <style>
             @media (max-width: 3199px) {
                 .img-bg{
@@ -56,7 +46,7 @@
     @show
 
     @section('scripts')
-
+        @include('trackers.gtm')
     @show
 </head>
 <body class="graybg">
