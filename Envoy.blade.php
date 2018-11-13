a31981297705431b79e60470ce4a643f26ddc317
Sebastian Barale <sebasinar@gmail.com>
2018-11-13 10:23:00 -0300

    removed slack

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -48,7 +48,7 @@ put_app_up
 @endmacro
 
 @after
-@slack($slack_hook, $slack_channel, "Envoy task $task ran on <$project_url|[$project_name]>")
+{{--@slack($slack_hook, $slack_channel, "Envoy task $task ran on <$project_url|[$project_name]>")--}}
 @endafter
 
 @task('version', ['on' => 'localhost'])
