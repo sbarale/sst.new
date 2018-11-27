d070c86d76c6e50c03988bbba017c1917d123024
Sebastian Barale <sebasinar@gmail.com>
2018-11-27 08:18:09 -0300

    changed id

--- resources/views/fronts/sst/_common/hidden_fields.blade.php
+++ resources/views/fronts/sst/_common/hidden_fields.blade.php
@@ -5,7 +5,7 @@
 <input id="leadid_token" name="universal_leadid" type="hidden" value=""/>
 <input type="hidden" name="click_id" id="click_id" value="{{$click_id}}"/>
 <input type="hidden" name="sub_id" id="sub_id" value="{{$sub_id}}"/>
-<input type="hidden" name="lp_request_id" id="sub_id" value="{{$lp_request_id}}"/>
+<input type="hidden" name="lp_request_id" id="lp_request_id" value="{{$lp_request_id}}"/>
 <input type="hidden" id="debug" name="debug" value="{{$debug}}">
 <input type="hidden" id="dev" name="dev" value="{{$dev}}">
 <input type="hidden" id="is_test" name="is_test" value="{{$test}}">
